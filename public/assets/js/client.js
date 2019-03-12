72c8532adb1b6e7439c1465fabcb9151c20e9da2
vaishalisaseenthar <44808887+vaishalisaseenthar@users.noreply.github.com>
2019-03-12 16:53:35 -0400

    Update client.js

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,7 +1,49 @@
-function init() {
-  $('#makeMeDraggable').draggable( {
-    containment: '#column middle',
-    cursor: 'move',
-    snap: '#content'
-  } );
-}
+var itemContainers = [].slice.call(document.querySelectorAll('.board-column-content'));
+var columnGrids = [];
+var boardGrid;
+
+itemContainers.forEach(function (container) {
+
+  var grid = new Muuri(container, {
+    items: '.board-item',
+    layoutDuration: 400,
+    layoutEasing: 'ease',
+    dragEnabled: true,
+    dragSort: function () {
+      return columnGrids;
+    },
+    dragSortInterval: 0,
+    dragContainer: document.body,
+    dragReleaseDuration: 400,
+    dragReleaseEasing: 'ease'
+  })
+  .on('dragStart', function (item) {
+    item.getElement().style.width = item.getWidth() + 'px';
+    item.getElement().style.height = item.getHeight() + 'px';
+  })
+  .on('dragReleaseEnd', function (item) {
+    item.getElement().style.width = '';
+    item.getElement().style.height = '';
+    columnGrids.forEach(function (grid) {
+      grid.refreshItems();
+    });
+  })
+  .on('layoutStart', function () {
+    boardGrid.refreshItems().layout();
+  });
+
+  columnGrids.push(grid);
+
+});
+
+boardGrid = new Muuri('.board', {
+  layoutDuration: 400,
+  layoutEasing: 'ease',
+  dragEnabled: true,
+  dragSortInterval: 0,
+  dragStartPredicate: {
+    handle: '.board-column-header'
+  },
+  dragReleaseDuration: 400,
+  dragReleaseEasing: 'ease'
+});
